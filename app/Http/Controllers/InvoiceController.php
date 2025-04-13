adcf13db86c2eae1be145ba8149906edd293a94e
zielu92 <zielu0@gmail.com>
2025-04-13 13:03:20 +0200

    feat: small changes

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -16,15 +16,20 @@ class InvoiceController extends Controller
         $items = $invoice->invoiceItems;
         $template = 'default';
         $paymentMethod = PaymentMethodsManager::getPaymentMethodTemplate(strtolower($invoice->paymentMethod->method), $invoice->paymentMethod->id);
-        return Pdf::view('invoice.template.'.$template.'.pdf', [
-            'invoice'       => $invoice,
-            'items'         => $items,
-            'showQty'       => $items->sum('quantity') !== count($items),
-            'showDiscount'  => $items->sum('total_discount') > 0,
+        return Pdf::view('invoice.template.' . $template . '.pdf', [
+            'invoice' => $invoice,
+            'items' => $items,
+            'showQty' => $items->sum('quantity') !== count($items),
+            'showDiscount' => $items->sum('total_discount') > 0,
             'paymentMethod' => $paymentMethod,
         ])
             ->format('a4')
-        ->name("#{$invoice->no}");
+            ->name("#{$invoice->no}")
+            ->withBrowsershot(function (Browsershot $browsershot) {
+                $browsershot->noSandbox()
+                    ->setChromePath('/home/sail/.cache/puppeteer/chrome/linux_arm-135.0.7049.84/chrome-linux64/chrome');
+
+            });
 
     }
 }
